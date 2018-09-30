e088280e865d6d12f590e7be58d482db20a7be13
muchamad2 <muhamadirsad146@gmail.com>
2018-10-01 00:58:49 +0700

    T.01.Programming_Update
    Update untuk tugas 01

---
+++ Apollo/Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float fuel;
+    private bool isFuelUse;
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        isFuelUse = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && fuel > 0)
+        {
+            rb.velocity = new Vector2(3, rb.velocity.y);
+            isFuelUse = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            isFuelUse = false;
+        }
+        if (isFuelUse && fuel > 0)
+        {
+            fuel-= 1f;
+        }
+	}
+}
---
+++ Apollo/Assets/Script/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour {
+    public Slider fuelSlide;
+    public PlayerController player;
+    // Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+        if(player != null)
+        {
+            fuelSlide.maxValue = player.fuel;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fuelSlide.value = player.fuel;
+	}
+}
